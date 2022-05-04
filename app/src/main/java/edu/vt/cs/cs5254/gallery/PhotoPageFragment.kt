829b7b39e44f7f1333b9ccf59915c632be4bdf11
Pei Cheng Huang <peicheng@peidembp.myfiosgateway.com>
2022-05-03 23:26:46 -0400

    implement ProgressBar

--- app/src/main/java/edu/vt/cs/cs5254/gallery/PhotoPageFragment.kt
+++ app/src/main/java/edu/vt/cs/cs5254/gallery/PhotoPageFragment.kt
@@ -6,8 +6,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 
 private const val ARG_URI = "photo_page_url"
@@ -15,6 +18,8 @@ private const val ARG_URI = "photo_page_url"
 class PhotoPageFragment: Fragment() {
     private lateinit var uri: Uri
     private lateinit var webView: WebView
+    private lateinit var progressBar: ProgressBar
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,9 +34,26 @@ class PhotoPageFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_photo_page, container, false)
+        progressBar = view.findViewById(R.id.progress_bar)
+        progressBar.max = 100
 
         webView = view.findViewById(R.id.web_view)
         webView.settings.javaScriptEnabled = true
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(webView: WebView, newProgress: Int) {
+                if (newProgress == 100) {
+                    progressBar.visibility = View.GONE
+                } else {
+                    progressBar.visibility = View.VISIBLE
+                    progressBar.progress = newProgress
+                }
+            }
+
+            override fun onReceivedTitle(view: WebView?, title: String?) {
+                (activity as AppCompatActivity).supportActionBar?.subtitle = title
+            }
+        }
+
         webView.webViewClient = WebViewClient()
         webView.loadUrl(uri.toString())
 
